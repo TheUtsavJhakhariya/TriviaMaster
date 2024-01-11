1b306113389fdf1edcc40ef5979a916b4b362e8a
Utsav Jhakhariya <86795590+TheUtsavJhakhariya@users.noreply.github.com>
2024-01-11 15:24:37 -0500

    Update triviamaster.js

--- triviamaster.js
+++ triviamaster.js
@@ -8,7 +8,7 @@
     },
     {
         question: "Who is the main antagonist in the movie 'Avengers: Infinity War'?",
-        choices: ["Loki", "Thanos", "Ultron", "Red Skull"], 
+        choices: ["Loki", "Thanos", "Ultron", "Red Skull"],  
         correctAnswer: "Thanos" 
     },
 
