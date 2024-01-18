d3381f9a5628097dfec596c99d5259c7c3eeda6d
Utsav Jhakhariya <86795590+TheUtsavJhakhariya@users.noreply.github.com>
2024-01-18 13:34:12 -0500

    Update triviamaster.js

--- triviamaster.js
+++ triviamaster.js
@@ -12,7 +12,7 @@
         correctAnswer: "Thanos" 
     },
   
-    {
+    { 
         question: "Which of the following superheroes wields a shield made of vibranium?",
         choices: ["Thor", "Hawkeye", "Black Widow", "Captain America"], 
         correctAnswer: "Captain America" 
